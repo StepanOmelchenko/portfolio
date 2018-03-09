c5be02841358930d052205bc92a8fc61205f2135
Stepan <omelchenko.stepan@gmail.com>
2018-03-09 19:54:43 +0600

    #1 gulp pug added

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,33 @@ const gulp = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
 const rename = require('gulp-rename');
+const pug = require('gulp-pug');
 
+const paths = {
+    root: './build',
+    templates: {
+        pages: './src/templates/pages/*.pug'
+    },
+    styles: {
+        main: './src/scss/main.scss',
+        dest: './build/css'
+    }
+};
+
+function templates() {
+    return gulp.src(paths.templates.pages)
+        .pipe(pug({pretty: true}))
+        .pipe(gulp.dest(paths.root));
+}
 
 function styles() {
-    return gulp.src('./src/scss/main.scss')
+    return gulp.src(paths.styles.main)
         .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(sourcemaps.write())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('./build/css'))
+        .pipe(gulp.dest(paths.styles.dest));
 }
 
-exports.styles = styles;
+exports.scss = styles;
+exports.pug = templates;
