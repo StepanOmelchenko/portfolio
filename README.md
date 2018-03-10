ff738ee13d81e310ee358f2774b692f77eab4d4b
Stepan <omelchenko.stepan@gmail.com>
2018-03-10 23:44:19 +0600

    #1 welcome page rebuild through gulp

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-Stepan Omelchenko
-
+made by Stepan Omelchenko
 portfolio website
+
+to install:
+
+clone this repo
+npm i
+gulp
--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
 const rename = require('gulp-rename');
 const pug = require('gulp-pug');
+const del = require('del');
+const imagemin = require('gulp-imagemin');
 
 const paths = {
     root: './build',
@@ -12,8 +14,20 @@ const paths = {
     styles: {
         main: './src/scss/main.scss',
         dest: './build/css'
+    },
+    images: {
+        src: './src/img/**/*.*',
+        dest: './build/img'
+    },
+    fonts: {
+        src: './src/fonts/**/*.*',
+        dest: './build/fonts'
     }
 };
+
+function clear() {
+    return del(paths.root);
+}
 
 function templates() {
     return gulp.src(paths.templates.pages)
@@ -30,5 +44,29 @@ function styles() {
         .pipe(gulp.dest(paths.styles.dest));
 }
 
+function images() {
+    return gulp.src(paths.images.src)
+        .pipe(imagemin({                //сомнительный эффект 
+            interlaced: true,
+            progressive: true,
+            optimizationLevel: 5,
+            svgoPlugins: [{removeViewBox: true}]
+        }))
+        .pipe(gulp.dest(paths.images.dest));
+}
+
+function fonts() {
+    return gulp.src(paths.fonts.src)
+        .pipe(gulp.dest(paths.fonts.dest));
+}
+
 exports.scss = styles;
 exports.pug = templates;
+exports.img = images;
+exports.delRoot = clear;
+exports.fonts = fonts;
+
+gulp.task('default', gulp.series(
+    clear,
+    gulp.parallel(styles, templates, images, fonts)
+));
