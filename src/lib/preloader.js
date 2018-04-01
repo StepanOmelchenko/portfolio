6c5edf840e1a647b52ec7a577263fdfa32f8dc99
Stepan <omelchenko.stepan@gmail.com>
2018-04-01 18:03:29 +0600

    #4 preloader ready

--- src/lib/preloader.js
+++ src/lib/preloader.js
@@ -1,8 +1,52 @@
-const preloader = document.querySelector('#preloader');
-//const preloaderPercent = document.querySelector('#preloader-percent');
+const preloader = (function () {
+    let precentTotal = 0;
+    let preloader = document.querySelector('#preloader');
+    let preloderTitle = document.querySelector('#preloader-title');
+    let paths = [];
+    let imgs = document.querySelectorAll('img').forEach((img) => {
+        paths.push(img.src);
+    }); 
+    let backGrounds = document.querySelectorAll('*').forEach((elem) => {
+        let bg = getComputedStyle(elem).getPropertyValue('background-image');
+        if (bg && bg !== 'none') {
+            paths.push(bg.replace('url("', '').replace('")', ''));
+        }
 
-if (preloader) {
-    document.body.onload = () => {
+    });
+    if (paths.length) {
+        paths.forEach((path) => {
+            imgLoader(path).then(() => {
+                precentTotal++;
+                setPercent(paths.length, precentTotal);
+            });
+        });
+    } else {
         preloader.classList.add('preloader--hide');
-    };
-}
+    }
+
+    function imgLoader(path) {
+        return new Promise(function(resolve) {
+            let fakeImg = document.createElement('img');
+            fakeImg.src = path;
+
+            fakeImg.addEventListener('load', () => {
+                resolve();
+            });
+            fakeImg.addEventListener('error', () => {
+                resolve();
+            });
+            
+        });
+    }
+
+    function setPercent(total, current) {
+        let percents = Math.ceil(current / total * 100);
+
+        preloderTitle.innerText = percents + '%';
+
+        if (percents >= 100) {
+            preloader.classList.add('preloader--hide');
+        }
+    }
+        
+})();
